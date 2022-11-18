52738b95c2beff168c0cf076e9064b904658bafd
Matheus Entringer <matheusentringer1@gmail.com>
2022-11-18 17:33:14 -0300

    Finished Homepage

---
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -0,0 +1,70 @@
+import { Visibility } from '@mui/icons-material'
+import React from 'react'
+import "./widgetsmall.css"
+
+const WidgetSmall = () => {
+  return (
+    <div className="widgetSmall">
+      <span className="widgetSmallTitle">New Join Members</span>
+      <ul className="widgetSmallList">
+        <li className="widgetSmallListItem">
+          <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallButton">
+            <Visibility className="widgetSmallIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallButton">
+            <Visibility className="widgetSmallIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallButton">
+            <Visibility className="widgetSmallIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallButton">
+            <Visibility className="widgetSmallIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmallListItem">
+          <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUsername">Anna Keller</span>
+            <span className="widgetSmallUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmallButton">
+            <Visibility className="widgetSmallIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default WidgetSmall
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,18 @@ import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredinfo/FeaturedInfo'
 import "./home.css"
 import { userData } from "../../dummyData"
+import WidgetSmall from '../../components/widgetSmall/WidgetSmall'
+import WidgetLarge from '../../components/widgetLarge/WidgetLarge'
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo/>
       <Chart title={"User Analytics"} data={userData} dataKey={"Active User"} grid={true} />
+      <div className="homeWidget">
+        <WidgetSmall />
+        <WidgetLarge />
+      </div>
     </div>
   )
 }
